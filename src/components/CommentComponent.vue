63f66994e3dc024478c027f3a5fa32e47e74c362
Mathéo DELAUNAY <dseonay@gmail.com>
2023-12-01 12:23:11 +0100

    Add CommentComponent.vue

---
+++ src/components/CommentComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="comment">
+    {{ comment }}
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'CommentComponent',
+    props: {
+        comment: String
+    }
+}
+</script>
+
+<style scoped>
+.comment {
+  background-color: #f2f2f2;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
