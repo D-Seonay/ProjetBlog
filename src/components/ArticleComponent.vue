73cb58b0f50488d5bfefa23d546eccc4bb803be2
Mathéo DELAUNAY <dseonay@gmail.com>
2023-12-01 12:23:15 +0100

    Add CommentComponent to ArticleComponent

--- src/components/ArticleComponent.vue
+++ src/components/ArticleComponent.vue
@@ -1,17 +1,19 @@
 <template>
-
     <h2>{{ article.title }}</h2>
     <p>{{ article.content }}</p>
-
+    <CommentComponent v-bind:comment="article.comment" />
 </template>
 
 <script>
+import CommentComponent from './CommentComponent.vue';
 
-    export default {
-        name: 'ArticleComponent',
-        props: {
-            article: Object
-        }
+export default {
+    name: 'ArticleComponent',
+    components: {
+        CommentComponent
+    },
+    props: {
+        article: Object
     }
-
+}
 </script>
